ac701e7729f7cd47cec645091454f76cf3b7e78c
steepnicki <steepnicki@gmail.com>
2022-04-01 20:04:06 +0100

    chore: Update git.ignore and ensure the script uses a correct version of validity

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-var validity = require('validity')
+var validity = require('@clocklimited/validity')
 
 module.exports = validity.createValidatorAllowingFailureMessageOverride(
   validity.booleanToCallback(isValid), '#{name} is required')
